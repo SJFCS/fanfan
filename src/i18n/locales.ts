WJZ-P/sona

feat: 壁纸头像分开路经wallpapers和avatars

src/i18n/locales.ts
@@ -225,22 +225,22 @@ export const zhCN = {
   'beautify.avatarMode.title': '自定义头像',
   'beautify.avatarMode.description': '启用后使用下方选择的图片替换客户端头像。关闭后保留头像资源列表，但不再应用自定义头像。',
   'beautify.avatar.inputTitle': '添加自定义头像',
-  'beautify.avatar.inputDescription': '输入 assets 内的图片资源路径。自定义头像仅支持图片。',
+  'beautify.avatar.inputDescription': '输入 avatars 目录内的图片资源路径。自定义头像仅支持图片。',
   'beautify.avatar.dropHint': '从下方资源列表拖动图片到这里，以添加自定义头像',
   'beautify.avatar.adjustTitle': '调整自定义头像',
   'beautify.avatar.adjustHint': '拖动调整位置，滚轮缩放。圆形遮罩会显示最终头像效果。',
   'beautify.avatar.waitingSync': '正在等待同步头像',
   'beautify.avatar.syncing': '正在同步头像...',
-  'beautify.assets.openFolder': '打开 assets 目录',
+  'beautify.assets.openFolder': '打开资源目录',
   'beautify.assets.add': '录入资源',
   'beautify.assets.placeholder': '输入 assets 相对路径...',
-  'beautify.assets.instructions': '请输入 assets 目录下的图片或视频相对路径，例如 wallpaper.png 或 backgrounds/wallpaper.mp4。',
+  'beautify.assets.instructions': '请输入资源文件名或相对路径，头像会自动拼到 avatars，壁纸会自动拼到 wallpapers。',
   'beautify.assets.browserTitle': '资源列表',
   'beautify.assets.dragHint': '拖动图片到上方功能区即可复制使用',
   'beautify.assets.folderTitle': '资源目录',
-  'beautify.assets.folderDescription': '打开 Sona 的 assets 目录，你的自定义图片、视频等资源应该放在这里。',
+  'beautify.assets.folderDescription': '打开 Sona 的资源目录，头像放 avatars，壁纸放 wallpapers。',
   'beautify.assets.inputTitle': '录入资源',
-  'beautify.assets.inputDescription': '输入相对于 assets 目录的图片或视频路径，Sona 会保存到资源列表并展示预览。',
+  'beautify.assets.inputDescription': '输入文件名或相对路径，Sona 会按当前功能自动补全 avatars 或 wallpapers。',
   'beautify.assets.examplePlaceholder': '例如 avatar.png',
   'beautify.assets.empty': '还没有录入资源。',
   'beautify.status.assetInputRequired': '请输入资源路径。',
@@ -662,22 +662,22 @@ export const enUS: Record<keyof typeof zhCN, string> = {
   'beautify.avatarMode.title': 'Custom Avatar',
   'beautify.avatarMode.description': 'Use the selected image below as the client avatar. Turning this off keeps the avatar list but stops applying custom avatars.',
   'beautify.avatar.inputTitle': 'Add Custom Avatar',
-  'beautify.avatar.inputDescription': 'Enter an image path inside assets. Custom avatars support images only.',
+  'beautify.avatar.inputDescription': 'Enter an image path inside avatars. Custom avatars support images only.',
   'beautify.avatar.dropHint': 'Drag images here from the asset list below',
   'beautify.avatar.adjustTitle': 'Adjust Custom Avatar',
   'beautify.avatar.adjustHint': 'Drag to reposition. Use mouse wheel to zoom. The circular mask shows the final avatar.',
   'beautify.avatar.waitingSync': 'Waiting to sync avatar',
   'beautify.avatar.syncing': 'Syncing avatar...',
-  'beautify.assets.openFolder': 'Open assets folder',
+  'beautify.assets.openFolder': 'Open folder',
   'beautify.assets.add': 'Add Asset',
   'beautify.assets.placeholder': 'Asset relative path...',
-  'beautify.assets.instructions': 'Enter an image or video path relative to the assets folder, e.g. wallpaper.png or backgrounds/wallpaper.mp4.',
+  'beautify.assets.instructions': 'Enter a file name or relative path. Avatars are resolved under avatars, wallpapers under wallpapers.',
   'beautify.assets.browserTitle': 'Assets',
   'beautify.assets.dragHint': 'Drag images to the sections above to use them',
   'beautify.assets.folderTitle': 'Assets Folder',
-  'beautify.assets.folderDescription': 'Open Sona assets folder. Put your custom images and videos here.',
+  'beautify.assets.folderDescription': 'Open the Sona asset folder. Put avatars in avatars and wallpapers in wallpapers.',
   'beautify.assets.inputTitle': 'Add Asset',
-  'beautify.assets.inputDescription': 'Enter an image or video path relative to assets. Sona will save it and show a preview.',
+  'beautify.assets.inputDescription': 'Enter a file name or relative path. Sona will prepend avatars or wallpapers for the current section.',
   'beautify.assets.examplePlaceholder': 'e.g. avatar.png',
   'beautify.assets.empty': 'No assets added yet.',
   'beautify.status.assetInputRequired': 'Please enter an asset path.',

SJFCS/fanfan

src/i18n/locales.ts
@@ -222,22 +222,22 @@ export const zhCN = {
   'beautify.avatarMode.title': '自定义头像',
   'beautify.avatarMode.description': '启用后使用下方选择的图片替换客户端头像。关闭后保留头像资源列表，但不再应用自定义头像。',
   'beautify.avatar.inputTitle': '添加自定义头像',
-  'beautify.avatar.inputDescription': '输入 assets 内的图片资源路径。自定义头像仅支持图片。',
+  'beautify.avatar.inputDescription': '输入 avatars 目录内的图片资源路径。自定义头像仅支持图片。',
   'beautify.avatar.dropHint': '从下方资源列表拖动图片到这里，以添加自定义头像',
   'beautify.avatar.adjustTitle': '调整自定义头像',
   'beautify.avatar.adjustHint': '拖动调整位置，滚轮缩放。圆形遮罩会显示最终头像效果。',
   'beautify.avatar.waitingSync': '正在等待同步头像',
   'beautify.avatar.syncing': '正在同步头像...',
-  'beautify.assets.openFolder': '打开 assets 目录',
+  'beautify.assets.openFolder': '打开资源目录',
   'beautify.assets.add': '录入资源',
   'beautify.assets.placeholder': '输入 assets 相对路径...',
-  'beautify.assets.instructions': '请输入 assets 目录下的图片或视频相对路径，例如 wallpaper.png 或 backgrounds/wallpaper.mp4。',
+  'beautify.assets.instructions': '请输入资源文件名或相对路径，头像会自动拼到 avatars，壁纸会自动拼到 wallpapers。',
   'beautify.assets.browserTitle': '资源列表',
   'beautify.assets.dragHint': '拖动图片到上方功能区即可复制使用',
   'beautify.assets.folderTitle': '资源目录',
-  'beautify.assets.folderDescription': '打开 FanFan 的 assets 目录，你的自定义图片、视频等资源应该放在这里。',
+  'beautify.assets.folderDescription': '打开 FanFan 的资源目录，头像放 avatars，壁纸放 wallpapers。',
   'beautify.assets.inputTitle': '录入资源',
-  'beautify.assets.inputDescription': '输入相对于 assets 目录的图片或视频路径，FanFan 会保存到资源列表并展示预览。',
+  'beautify.assets.inputDescription': '输入文件名或相对路径，FanFan 会按当前功能自动补全 avatars 或 wallpapers。',
   'beautify.assets.examplePlaceholder': '例如 avatar.png',
   'beautify.assets.empty': '还没有录入资源。',
   'beautify.status.assetInputRequired': '请输入资源路径。',
@@ -656,22 +656,22 @@ export const enUS: Record<keyof typeof zhCN, string> = {
   'beautify.avatarMode.title': 'Custom Avatar',
   'beautify.avatarMode.description': 'Use the selected image below as the client avatar. Turning this off keeps the avatar list but stops applying custom avatars.',
   'beautify.avatar.inputTitle': 'Add Custom Avatar',
-  'beautify.avatar.inputDescription': 'Enter an image path inside assets. Custom avatars support images only.',
+  'beautify.avatar.inputDescription': 'Enter an image path inside avatars. Custom avatars support images only.',
   'beautify.avatar.dropHint': 'Drag images here from the asset list below',
   'beautify.avatar.adjustTitle': 'Adjust Custom Avatar',
   'beautify.avatar.adjustHint': 'Drag to reposition. Use mouse wheel to zoom. The circular mask shows the final avatar.',
   'beautify.avatar.waitingSync': 'Waiting to sync avatar',
   'beautify.avatar.syncing': 'Syncing avatar...',
-  'beautify.assets.openFolder': 'Open assets folder',
+  'beautify.assets.openFolder': 'Open folder',
   'beautify.assets.add': 'Add Asset',
   'beautify.assets.placeholder': 'Asset relative path...',
-  'beautify.assets.instructions': 'Enter an image or video path relative to the assets folder, e.g. wallpaper.png or backgrounds/wallpaper.mp4.',
+  'beautify.assets.instructions': 'Enter a file name or relative path. Avatars are resolved under avatars, wallpapers under wallpapers.',
   'beautify.assets.browserTitle': 'Assets',
   'beautify.assets.dragHint': 'Drag images to the sections above to use them',
   'beautify.assets.folderTitle': 'Assets Folder',
-  'beautify.assets.folderDescription': 'Open FanFan assets folder. Put your custom images and videos here.',
+  'beautify.assets.folderDescription': 'Open the FanFan asset folder. Put avatars in avatars and wallpapers in wallpapers.',
   'beautify.assets.inputTitle': 'Add Asset',
-  'beautify.assets.inputDescription': 'Enter an image or video path relative to assets. FanFan will save it and show a preview.',
+  'beautify.assets.inputDescription': 'Enter a file name or relative path. FanFan will prepend avatars or wallpapers for the current section.',
   'beautify.assets.examplePlaceholder': 'e.g. avatar.png',
   'beautify.assets.empty': 'No assets added yet.',
   'beautify.status.assetInputRequired': 'Please enter an asset path.',